cee40d99bfc961ff5600498d1f57a97c6dbc1da1
Divya <Divss185@gmail.com>
2025-07-09 02:20:35 +0530

    Initial project structure

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+  return (
+    <div className="main-content">
+      {/* Your main feed content will go here */}
+      <h1>Feed</h1>
+    </div>
+  );
+};
+
+export default MainContent;
---
+++ src/components/RightPanel.jsx
@@ -0,0 +1,27 @@
+const RightPanel = () => {
+  return (
+    <div className="right-panel">
+      <div className="section">
+        <h6>OUTLINE TIMELINE</h6>
+        <p>master 0 A 0</p>
+      </div>
+      
+      <div className="section">
+        <h6>PIC</h6>
+        <p>Mostly cloudy</p>
+      </div>
+      
+      <div className="section">
+        <h6>SINCE</h6>
+        <p>01/09/2025</p>
+      </div>
+      
+      <div className="time-display">
+        <p>08:00</p>
+        <p>02:22</p>
+      </div>
+    </div>
+  );
+};
+
+export default RightPanel;
