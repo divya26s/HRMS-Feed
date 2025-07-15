472d15ca4a66b93f1dc8d846694ed1a46f5da313
Abhisar Sharma <abhisarsharma2006@gmail.com>
2025-07-15 22:49:59 +0530

    rigth panel css

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -1,26 +1,25 @@
 const RightPanel = () => {
   return (
     <div className="right-panel">
-      <div className="section">
-        <h6>OUTLINE TIMELINE</h6>
-        <p>master 0 A 0</p>
-      </div>
-      
-      <div className="section">
-        <h6>PIC</h6>
-        <p>Mostly cloudy</p>
-      </div>
-      
-      <div className="section">
-        <h6>SINCE</h6>
-        <p>01/09/2025</p>
-      </div>
-      
-      <div className="time-display">
-        <p>08:00</p>
-        <p>02:22</p>
-      </div>
-    </div>
+      {/*1, Upcoming Events */}
+      <div className="Widget">
+        <h3>Upcoming Events</h3>
+        <ul className="event-list">
+          <li><strong>team sync</strong> -Today ,2:30 PM</li>
+          <li><strong>Town hall</strong> -Friday ,7:45 PM</li>
+        </ul>
+        {/* 2, Quick Action */}
+              <div className="Widget">
+                <h3> Ouick Action </h3>
+                <div className="quick-action">
+                  <button className="action-button">Mark Attendance</button>
+                  <button className="action-button">view payslip</button>
+                  
+                  </div>
+
+</div>
+        </div>
+        </div>
   );
 };
 
